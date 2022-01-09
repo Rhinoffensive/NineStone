c8e19bc62a6ca92a93c166a1e9c832326dcfd433
Rπ <renantaylan@gmail.com>
2022-01-09 18:05:58 +0300

    Movement and snap to tile

--- Assets/_Scripts/GridManager.cs
+++ Assets/_Scripts/GridManager.cs
@@ -4,66 +4,102 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-   
-    [SerializeField] public int _width,_height;
+
+    [SerializeField] public int _width, _height;
     [SerializeField] private Tile _tilePrefab;
     //[SerializeField] private GridManager _board;
     [SerializeField] private Transform _cam;
     [SerializeField] private Piece white_piece;
-     [SerializeField] private Piece black_piece;
+    [SerializeField] private Piece black_piece;
 
-    
+
 
     private Dictionary<Vector2, Tile> _tiles;
+    private List<Piece> _pieces;
 
-    private void Start() {
-           GenerateGrid();
-          
-           GeneratePieces();
-}
-    void GenerateGrid(){
-        _tiles = new Dictionary<Vector2, Tile>();
-        for(int x=0;x<_width;x++){
-            for(int y=0;y<_height;y++){
-                var spawnedTile = Instantiate(_tilePrefab,new Vector3(x,y,2),Quaternion.identity);
-                spawnedTile.name = $"Tile {x} {y}";
 
-                var isOffset= (x%2==0 && y %2!=0) || (x%2!=0 && y%2==0);
-                spawnedTile.Init(isOffset);   
+    private void Start()
+    {
+        GenerateGrid();
 
-                _tiles[new Vector2(x,y)] = spawnedTile;           
-
-            }
-        }
-
-        _cam.transform.position = new Vector3((float)_width/2-0.5f,(float)_height/2-0.5f,-10f);
+        GeneratePieces();
     }
 
-    void GeneratePieces(){
-        for(int x=0;x<3;x++){
-            for(int y=0;y<3;y++){
-                var tile = GetTileAtPositon(new Vector2(x,y));
-                var piece = Instantiate(white_piece,new Vector3(x,y),Quaternion.identity);
-                piece.transform.parent = tile.transform;
+    private void FixedUpdate()
+    {      
+        foreach(Piece piece in _pieces){
+            var piece_pos = piece.transform.position;
+            piece.transform.position = new Vector3(Mathf.RoundToInt(piece_pos.x),Mathf.RoundToInt(piece_pos.y));
+            var tile = GetTileAtPositon(new Vector2(Mathf.RoundToInt(piece_pos.x),Mathf.RoundToInt(piece_pos.y)));
+            piece.transform.SetParent(tile.transform);
+        }
+    }
+
+
+
+   
+    void GenerateGrid()
+    {
+        _tiles = new Dictionary<Vector2, Tile>();
+        for (int x = 0; x < _width; x++)
+        {
+            for (int y = 0; y < _height; y++)
+            {
+                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y, 1), Quaternion.identity);
+                spawnedTile.name = $"Tile {x} {y}";
+
+                var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
+                spawnedTile.Init(isOffset);
+
+                _tiles[new Vector2(x, y)] = spawnedTile;
+
+                spawnedTile.transform.SetParent(this.transform);
+              
+
             }
         }
 
-           for(int x=7;x>4;x--){
-            for(int y=7;y>4;y--){
-                var tile = GetTileAtPositon(new Vector2(x,y));
-                var piece = Instantiate(black_piece,new Vector3(x,y),Quaternion.identity);
-                piece.transform.parent = tile.transform;
+        _cam.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f, -10f);
+    }
+
+    void GeneratePieces()
+    {
+
+        _pieces = new List<Piece>();
+        for (int x = 0; x < 3; x++)
+        {
+            for (int y = 0; y < 3; y++)
+            {
+                var tile = GetTileAtPositon(new Vector2(x, y));
+                var piece = Instantiate(white_piece, new Vector3(x, y), Quaternion.identity);                
+                piece.transform.SetParent(tile.transform);
+                _pieces.Add(piece);
+            }
+        }
+
+        for (int x = 7; x > 4; x--)
+        {
+            for (int y = 7; y > 4; y--)
+            {
+                var tile = GetTileAtPositon(new Vector2(x, y));
+                var piece = Instantiate(black_piece, new Vector3(x, y), Quaternion.identity);
+                piece.transform.SetParent(tile.transform);
+                _pieces.Add(piece);
             }
         }
 
 
-}
-    public Tile GetTileAtPositon(Vector2 pos){
-        if(_tiles.TryGetValue(pos, out var tile)){
+    }
+    public Tile GetTileAtPositon(Vector2 pos)
+    {
+        if (_tiles.TryGetValue(pos, out var tile))
+        {
             return tile;
         }
         return null;
     }
+
+
 
 
 }
--- Assets/_Scripts/Piece.cs
+++ Assets/_Scripts/Piece.cs
@@ -3,41 +3,72 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public enum PieceColor{
+public enum PieceColor
+{
     white,
     black
 }
 public class Piece : MonoBehaviour
 {
     public PieceColor color;
-    [SerializeField]private GameObject highLight;
-    private bool isHighLighted = false;
+    [SerializeField] private GameObject highLight;
+
+    private GridManager gridManager;
+    private bool _dragging;
     private SpriteRenderer _renderer;
+
+    
     private void Start()
     {
         _renderer = GetComponent<SpriteRenderer>();
-        if(color == PieceColor.white)
+        if (color == PieceColor.white)
             _renderer.color = Color.white;
         else
             _renderer.color = Color.black;
 
-
-    }
-
-    private void OnMouseEnter()
-    {
-        isHighLighted = true;
-        if(!isHighLighted)
-            highLight.SetActive(true);
-    }
-
-    private void OnMouseExit()
-    {
+        gridManager =  FindObjectOfType<GridManager>();
         
-        isHighLighted = false;
-        if(isHighLighted)
-            highLight.SetActive(false);
+
+
     }
+
+    private void FixedUpdate() {
+        if(!_dragging) return;
+        var mousePosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePosition;          
+    }
+
+    private void OnMouseDown() {
+        _dragging = true;
+    }
+
+    private void OnMouseUp() {
+        _dragging = false;
+        
+        
+        var mousePosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePosition;
+
+    
+        
+
+    }
+
+    // private void OnCollisionEnter2D(Collision2D other) {
+    //     if(other.gameObject.tag == "Tile")
+    //     {
+    //         print(other.gameObject.name);
+    //     }
+    // }
+    // private void OnMouseEnter()
+    // {
+    //     highLight.SetActive(true);
+    // }
+
+    // private void OnMouseExit()
+    // {
+    //     highLight.SetActive(false);
+    // }
 
 
 
